0fae12252d35d721181d24d60e2890811ab35725
influensor <influensoros@gmail.com>
2026-02-18 10:51:10 +0530

    update

--- test_monitor.py
+++ test_monitor.py
@@ -1,12 +1,10 @@
 from engine.post_monitor.monitor import PostMonitor
-
 usernames = [
     "_immanzar_",
     "akashviraniphotography",
     "ashok__chaudhary___",
     "diyasingh_dynameets",
     "eternalbright.in",
-    "gauravkotharii",
     "muralisa_i",
     "naeemsyedofficial",
     "pixelwithprotein",
@@ -15,11 +13,8 @@ usernames = [
 ]
 
 monitor = PostMonitor(headless=True)
-
 results = monitor.check_multiple(usernames, limit=6)
-
 monitor.close()
-
 for username, posts in results.items():
     if posts:
         print(f"New posts for {username}:")
@@ -27,3 +22,4 @@ for username, posts in results.items():
             print(post)
     else:
         print(f"No new posts for {username}")
+
