d83fff98a8a6ae35294b8bc89e59b6ca5c985e5a
Abhijeet <blackaquaindia@gmail.com>
2026-07-28 23:01:02 +0530

    update

--- engine/post_monitor/monitor.py
+++ engine/post_monitor/monitor.py
@@ -14,13 +14,13 @@ from .storage_new import load_saved_posts, save_posts
 # =====================================================
 
 PROFILES = [
-    r"C:\instagram_a4agharkar.in",
+#    r"C:\instagram_a4agharkar.in",
     r"C:\instagram_abhijit.agharkar",
-    r"C:\instagram_blackaquaindia.in",
-#    r"C:\instagram_mimy_ai",
-#    r"C:\instagram_nashikonwheels",
+#    r"C:\instagram_blackaquaindia.in",
+    r"C:\instagram_mimy_ai",
+    r"C:\instagram_nashikonwheels",
     r"C:\instagram_shophudabeauty.in",
-    r"C:\instagram_shivdigitalofficial",
+#    r"C:\instagram_shivdigitalofficial",
 ]
 USER_DATA_DIR = random.choice(PROFILES)
 
