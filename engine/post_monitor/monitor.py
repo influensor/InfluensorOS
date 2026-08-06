4319c89d2d5dc04f0bc9b74e448b1fda1e582e3e
Abhijeet <blackaquaindia@gmail.com>
2026-08-06 16:37:34 +0530

    update

--- engine/post_monitor/monitor.py
+++ engine/post_monitor/monitor.py
@@ -14,7 +14,7 @@ from .storage_new import load_saved_posts, save_posts
 # =====================================================
 
 PROFILES = [
-    r"C:\instagram_a4agharkar.in",
+#    r"C:\instagram_a4agharkar.in",
     r"C:\instagram_abhijit.agharkar",
     r"C:\instagram_blackaquaindia.in",
     r"C:\instagram_mimy_ai",
