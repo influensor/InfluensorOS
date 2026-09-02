8019eb3256a8c957a75773893c642f8aada1de8f
Abhijeet <blackaquaindia@gmail.com>
2026-09-02 13:56:19 +0530

    update

--- test_monitor.py
+++ test_monitor.py
@@ -26,8 +26,10 @@ usernames = [
     "sana_glamgoddess",
     "shivanisharmafoundation",
     "smilesbydrravneetkaur",
+    "strangersindia.store",
     "swarnapraveen1",
     "techbyrawat",
+    "torqos.ev",
     "vanitas_payal_beauty999",
     "vickygetfit",
     "wander_bites_duo"
