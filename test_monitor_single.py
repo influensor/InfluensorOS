58b59ebd7e674a72b29fe71229d6de51c1621428
Abhijeet <blackaquaindia@gmail.com>
2026-09-02 13:43:28 +0530

    update

--- test_monitor_single.py
+++ test_monitor_single.py
@@ -5,7 +5,7 @@ import subprocess
 from engine.post_monitor.monitor import PostMonitor
 
 usernames = [
-    "smilesbydrravneetkaur",
+    "torqos.ev",
  ]
 
 #INDEX_FILE = r"C:\Users\yagha\OneDrive\Documents\GitHub\InfluensorOS\current_user.txt"
@@ -28,7 +28,7 @@ username = usernames[current_index]
 print(f"Processing: {username}")
 
 monitor = PostMonitor(headless=True)
-results = monitor.check_multiple([username], limit=45)
+results = monitor.check_multiple([username], limit=12)
 monitor.close()
 
 for username, posts in results.items():
