f1c26b452a5dc07394d7eb126f5680febe2e6e02
Influensor OS <influensoros@gmail.com>
2026-09-24 07:14:37 +0530

    update

--- engine/post_monitor/monitor.py
+++ engine/post_monitor/monitor.py
@@ -22,7 +22,7 @@ PROFILES = [
     r"C:\instagram_mimy_ai",
     r"C:\instagram_nashikonwheels",
     r"C:\instagram_saisumanglass",
-    r"C:\instagram_shivdigitalofficial",
+#    r"C:\instagram_shivdigitalofficial",
     r"C:\instagram_shivglasse",
     r"C:\instagram_shubh__5__",
 ]
